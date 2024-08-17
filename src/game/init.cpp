be6ad88a19d0ab80d51995948ae8a58e0781ffcd
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2024-08-17 16:30:44 +0300

    game: init: Add second player object

--- src/game/init.cpp
+++ src/game/init.cpp
@@ -11,6 +11,15 @@ namespace game
     vector<Player> players {
         Player{
             .area = {0, 0, 32, 64}
+        },
+        {
+            .area = {128, 0, 32, 64},
+            .controls = {
+                SDL_SCANCODE_UP,
+                SDL_SCANCODE_RIGHT,
+                SDL_SCANCODE_DOWN,
+                SDL_SCANCODE_LEFT
+            }
         }
     };
 
