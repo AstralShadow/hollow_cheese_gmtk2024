99a28ac6190f326829ce9dc1bc826a9bd627cf4d
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2024-08-18 22:33:11 +0300

    scenes: Cleanup

--- src/scenes.cpp
+++ src/scenes.cpp
@@ -9,16 +9,17 @@
 
 void core::register_scenes()
 {
-    core::scene<startup::scene_uid>("startup");
-    core::scene<game::scene_uid>("game"); // Only meant for mechanic prototyping
+    core::scene<game::scene_uid>(); // Only meant for mechanic prototyping; currently not working
     core::scene<world::scene_uid>();
+
+    core::scene<startup::scene_uid>("startup");
+    //core::scene<menu::scene_uid>("menu"); // TODO add
 
     core::scene<LE::scene_uid>("level_editor");
     core::scene<WE::scene_uid>("world_editor");
 
     core::set_scene("world_editor");
-    //core::set_scene("game");
-    core::set_scene("startup");
+    //core::set_scene("startup");
 }
 
 
