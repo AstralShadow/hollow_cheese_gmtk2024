2183d04a1355d34074618a1a48c03e2813b8abe1
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2024-08-20 01:30:07 +0300

    world: Remove test level

--- src/world/init.cpp
+++ src/world/init.cpp
@@ -11,24 +11,7 @@ namespace world
 
 void world::init(int, char**, scene_uid)
 {
-    const int size = 48;
 
-    Level level;
-    for(int i = 0; i < 20; i++)
-        level.tiles.push_back(Tile
-        {
-            .area = {size * i, 304, size, size},
-            .min_size = {16, 16},
-            .scalable = {(i & 1) == 1, (i & 2) == 2, (i & 4) == 4, (i & 8) == 8}
-        });
-
-    level.tiles[10].scalable[TOP] = true;
-    level.tiles[10].mandatory_area = level.tiles[10].area;
-
-    world.levels.push_back({
-        .level = level,
-        .pos = {0, 0}
-    });
 }
 
 void world::deinit(scene_uid)
