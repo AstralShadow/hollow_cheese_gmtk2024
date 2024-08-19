b1a4e1d0420140db392f4545eff7e34f360a471e
AzCraft <47869198+AstralShadow@users.noreply.github.com>
2024-08-20 01:57:32 +0300

    world_editor: Add no levels notice

--- src/world_editor/render.cpp
+++ src/world_editor/render.cpp
@@ -2,7 +2,13 @@
 #include "core/core.hpp"
 #include "world/render.hpp"
 #include "world_editor/data.hpp"
+#include "game/fonts.hpp"
+#include "utils/textures.hpp"
 #include <SDL2/SDL_render.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 static auto& rnd = core::renderer;
 
@@ -19,6 +25,42 @@ void WE::render(scene_uid)
         world::render_world(world::world, true);
 
         world::world.current_level = temp;
+    }
+
+    /* No levels notice */
+    if(world::world.levels.size() == 0)
+    {
+        static SDL_Texture* notice = nullptr;
+        static Rect area;
+        if(!notice)
+        {
+            string text = "Welcome to the World Editor\n\n"
+                          "There are no levels yet.\n"
+                          "Please use the button \n"
+                          "at the bottom right corner\n"
+                          "to create one.";
+
+            auto font = game::get_font();
+            SDL_Color white {255, 255, 255, 255};
+
+            auto surf = TTF_RenderUTF8_Blended_Wrapped(font, text.c_str(), white, 0);
+            if(!surf) {
+                cout << "Failed to render notice" << endl;
+                cout << TTF_GetError() << endl;
+                return;
+            }
+
+            notice = utils::create_texture(surf);
+            area = Rect {
+                (WINDOW_WIDTH - surf->w) / 2,
+                (WINDOW_HEIGHT - surf->h) / 2,
+                surf->w,
+                surf->h
+            };
+
+            SDL_FreeSurface(surf);
+        }
+        SDL_RenderCopy(rnd, notice, nullptr, &area);
     }
 
     if(mouse_focus.selected)
